9cbc656494329ad9a425ccf69399cd41f12540a6
raelrzd <israelrezendeluiz2903@gmail.com>
2025-01-07 12:39:06 -0300

    Criados os primeiros testes de unidade seguinte o padrão triple A para testar usuarios, tais como, verificações de usuario valido, email valido e senha forte.

---
+++ app/src/test/java/br/com/alura/orgs/TestaUsuario.kt
@@ -0,0 +1,53 @@
+package br.com.alura.orgs
+
+import br.com.alura.orgs.model.Usuario
+import org.junit.Assert
+import org.junit.Test
+
+class TestaUsuario {
+
+    @Test
+    fun retornaVerdadeiroSeOsDadosDoUsuarioEstaoCorretos() {
+        val usuarioValido = Usuario(
+            id = "usuario",
+            email = "usuario@valido.com",
+            senha = "senhaforte123"
+        )
+
+        val resultado = usuarioValido.ehValido()
+        Assert.assertTrue(resultado)
+    }
+
+    @Test
+    fun retornaFalsoSeOEmailDoUsuarioEhInvalido() {
+        // Arrange - Organizar
+        val usuarioInvalido = Usuario(
+            id = "usuario",
+            email = "emailinvalido.com",
+            senha = "senhaforte123"
+        )
+
+        // Act - Agir
+        val resultado = usuarioInvalido.ehValido()
+
+        // Assert - Afirmar
+        Assert.assertEquals(false, resultado)
+    }
+
+    @Test
+    fun retornaFalsoSeASenhaDoUsuarioEhFraca() {
+        // Arrange - Organizar
+        val usuarioInvalido = Usuario(
+            id = "usuario",
+            email = "email@valido.com",
+            senha = "senha"
+        )
+
+        // Act - Agir
+        val resultado = usuarioInvalido.ehValido()
+
+        // Assert - Afirmar
+        Assert.assertEquals(false, resultado)
+    }
+
+}
